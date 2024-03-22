4a99fb36907b5368daee41718e617ad8bcc72711
hencybhalani <hencybhalani@gmail.com>
2024-03-22 17:44:08 +0530

    Add project files.

---
+++ map_application/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace map_application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
